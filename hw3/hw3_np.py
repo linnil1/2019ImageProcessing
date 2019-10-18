70739321b0f43eabb9a35ae58de2ffeca1278c3e
linnil1 <linnil1.886@gmail.com>
2019-10-19 02:08:06 +0800

    Add filter functions

--- hw3/hw3_np.py
+++ hw3/hw3_np.py
@@ -55,15 +55,154 @@ def maxFilter(*args, **kwargs):
     return utils_cpp.maxFilter(*args, **kwargs)
 
 
+def spatialConv(img, krn):
+    # padding
+    size_img = np.array(img.shape)
+    size_krn = np.array(krn.shape)
+    size = np.max(np.stack([size_img, size_krn]), axis=0)
+    pad_img = np.pad(img, [(0, size[0] - size_img[0]),
+                           (0, size[1] - size_img[1])], 'constant')
+    pad_krn = np.pad(krn, [(0, size[0] - size_krn[0]),
+                           (0, size[1] - size_krn[1])], 'constant')
+
+    # convolute and inverse
+    result_feq = np.fft.fft2(pad_img) * np.fft.fft2(pad_krn)
+    result_sp = np.real(np.fft.ifft2(result_feq))[:size_img[0], :size_img[1]]
+    return result_sp
+
+
+def wrapSharpenFilter(func):
+    """
+    Wrap the unsharpen coefficient to all high pass filter
+    The custom function will return the convolution of img and it's kernal
+    """
+    @hw1.limitImg
+    def wrapFunc(img, k=0, needabs=True, *args, **kwagrs):
+        global tmp1
+        res = func(img, *args, **kwagrs)
+        if needabs:
+            res = np.abs(res)
+        return img + k * res
+    return wrapFunc
+
+
+@wrapSharpenFilter
+def sobelH(img):
+    kernal = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    return spatialConv(img, kernal)
+
+
+@wrapSharpenFilter
+def sobelV(img):
+    kernal = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    return spatialConv(img, kernal)
+
+
+@wrapSharpenFilter
+def laplacian4(img):
+    kernal = -np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    return spatialConv(img, kernal)
+
+
+@wrapSharpenFilter
+def laplacian8(img):
+    kernal = -np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
+    return spatialConv(img, kernal)
+
+
+@wrapSharpenFilter
+def unsharpen(img, sig):
+    """
+    Unsharpen method: high pass = ori - lowpass
+    """
+    return img - gaussian(img, sig)
+
+
+@wrapSharpenFilter
+def roberA(img):
+    kernal = np.array([[-1, 0], [0, 1]])
+    return spatialConv(img, kernal)
+
+
+@wrapSharpenFilter
+def roberB(img):
+    kernal = np.array([[0, -1], [1, 0]])
+    return spatialConv(img, kernal)
+
+
+def feqOperation(img, func):
+    """
+    Editing the map by func in frequency domain.
+    The input of func is a distance to center
+    """
+    # fft
+    trans_img = img.copy()
+    trans_img[0::2, 1::2] *= -1
+    trans_img[1::2, 0::2] *= -1
+    feq = np.fft.fft2(trans_img)
+
+    # distance and func
+    s = img.shape
+    j, i = np.meshgrid(np.arange(s[1]) - s[1] // 2,
+                       np.arange(s[0]) - s[0] // 2)
+    filter_feq = func(j ** 2 + i ** 2)
+
+    # inverse
+    filter_img = np.fft.ifft2(filter_feq * feq)
+    filter_img[0::2, 1::2] *= -1
+    filter_img[1::2, 0::2] *= -1
+    result_sp = np.real(filter_img)
+    return result_sp
+
+
+def gaussian(img, sig):
+    """
+    Gaussian Blur
+    """
+    def gaussianFilter(dest):
+        return np.exp(-dest / sig ** 2)
+    return feqOperation(img, gaussianFilter)
+
+
+def idealLow(img, cutoff):
+    """
+    Low pass filetr by ideal cutoff
+    """
+    def idealFilter(dest):
+        return dest < cutoff ** 2
+    return feqOperation(img, idealFilter)
+
+
+def butterWorth(img, cutoff, n):
+    """
+    ButterWorth low pass filter
+    """
+    def butterWorthFilter(dest):
+        return 1 / (1 + dest / (cutoff ** 2)) ** n
+    return feqOperation(img, butterWorthFilter)
+
+
 def test():
     # read
-    real_image = hw2.readRGB("data/Image 4-1.jpg")
+    real_image = hw2.readRGB("data/Image 3-2.JPG")
     gray_image = hw2.toGrayA(real_image)
-    a = medianFilter(gray_image, np.array([35, 35]))
-    import timeit
-    # print(timeit.timeit(lambda: minFilter(gray_image, np.array([5, 5])), number=10))
+    plt.figure(figsize=(10, 8))
+    plt.subplot(2, 2, 1)
+    plt.title("Original")
+    plt.imshow(gray_image, cmap="gray")
+    plt.subplot(2, 2, 2)
+    plt.title("Rober A")
+    a = roberA(gray_image, k=2, needabs=True)
+    plt.imshow(tmp, cmap="gray")
+    plt.subplot(2, 2, 3)
+    plt.title("Apply the filter")
+    plt.imshow(tmp1, cmap="gray")
+    plt.subplot(2, 2, 4)
+    plt.title("Apply the filter and add to original")
     plt.imshow(a, cmap="gray")
     plt.show()
+    # import timeit
+    # print(timeit.timeit(lambda: spatialConv(gray_image, kernal), number=10))
     exit()
 
 
